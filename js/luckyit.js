892eed1311cd9c91198e4678a3063c2abeed7dd6
haikuotianko <57618047+haikuotianko@users.noreply.github.com>
2020-07-11 14:31:03 +0800

    Add files via upload

---
+++ js/luckyit.js
@@ -0,0 +1,26 @@
+const luckyit = {}
+
+luckyit.json = function(src, obj){
+	return new Promise((resolve, reject) => {
+		const el = document.createElement('iframe')
+		obj ? src += '?' + Object.keys(obj).map(key => key + '=' + obj[key]) .join('&;') : '',
+		el.src = src
+		el.style.display = 'none'
+		window.document.body.appendChild(el)
+		el.onload = function() {
+			const body = el.contentWindow.document.body
+			const data = body.children[0].textContent
+			try{
+				resolve(JSON.parse(data))
+			}catch(e){
+				//TODO handle the exception
+				resolve(data)
+			}
+			el.remove()
+		}
+	})
+}
+
+
+// module.export = luckyit
+// export default luckyit
